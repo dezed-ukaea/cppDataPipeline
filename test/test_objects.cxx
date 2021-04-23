afdaa0756c3a7df94a571e27f576c630fd566d60
Kristian Zarebski <kristian.zarebski@ukaea.uk>
2021-04-23 10:21:06 +0100

    Added DataTable and test

---
+++ test/test_objects.cxx
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+#include "scrc/objects/data.hxx"
+
+using namespace SCRC;
+
+TEST(SCRCAPITest, TestDataTableCreate) {
+    DataTable* data_table_ = new DataTable;
+    data_table_->add_column<std::string>("Name", {"John", "James", "David", "George"});
+    data_table_->add_column<int>("Age", {34, 56, 54, 23});
+    data_table_->add_column<float>("Height", {182.3, 183.4, 179.1, 175.9}, "cm");
+    ASSERT_EQ((*data_table_->get_string_column("Name"))[2], "David");
+    std::cout << data_table_->print() << std::endl;
+}
