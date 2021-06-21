4a725bb24d283dcd804d7ce53b1b2539032ccb8c
Kristian Zarebski <kristian.zarebski@ukaea.uk>
2021-06-21 13:07:05 +0100

    Added find package macros

--- external/yaml_cpp.cmake
+++ external/yaml_cpp.cmake
@@ -1,16 +1,30 @@
 SET( YAML_CPP "yaml-cpp" )
-ExternalProject_Add(
-    ${YAML_CPP}_src
-    GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
-    GIT_TAG yaml-cpp-0.6.3
-    GIT_SHALLOW TRUE
-    INSTALL_COMMAND ""
-    CMAKE_CACHE_ARGS
-        -DYAML_CPP_BUILD_TESTS:BOOL=OFF
-        -DYAML_BUILD_SHARED_LIBS:BOOL=ON
-)
+find_package( yaml-cpp QUIET )
+SET( YAMLCPP_TAG "yaml-cpp-0.6.3" )
+SET( YAMLCPP_URL "https://github.com/jbeder/yaml-cpp.git" )
 
-ExternalProject_Get_Property( ${YAML_CPP}_src binary_dir )
-ExternalProject_Get_Property( ${YAML_CPP}_src source_dir )
-SET( YAML_CPP_INCLUDE_DIRS ${source_dir}/include )
-SET( YAML_CPP_LIBRARIES ${binary_dir}/lib${YAML_CPP}${CMAKE_SHARED_LIBRARY_SUFFIX} )
+MESSAGE( STATUS "[YAML-cpp]" )
+
+IF( NOT YAMLCPP_INCLUDE_DIR )
+    MESSAGE( STATUS "\tYAML-cpp not installed on this system and so will be downloaded." )
+    MESSAGE( STATUS "\tURL: ${YAMLCPP_URL}" )
+    MESSAGE( STATUS "\tTAG: ${YAMLCPP_TAG}" )
+    ExternalProject_Add(
+        ${YAML_CPP}_src
+        GIT_REPOSITORY ${YAMLCPP_URL}
+        GIT_TAG ${YAMLCPP_TAG}
+        GIT_SHALLOW TRUE
+        INSTALL_COMMAND ""
+        CMAKE_CACHE_ARGS
+            -DYAML_CPP_BUILD_TESTS:BOOL=OFF
+            -DYAML_BUILD_SHARED_LIBS:BOOL=ON
+    )
+
+    ExternalProject_Get_Property( ${YAML_CPP}_src binary_dir )
+    ExternalProject_Get_Property( ${YAML_CPP}_src source_dir )
+    SET( YAMLCPP_INCLUDE_DIR ${source_dir}/include )
+    SET( YAMLCPP_LIBRARY ${binary_dir}/lib${YAML_CPP}${CMAKE_SHARED_LIBRARY_SUFFIX} )
+ELSE()
+    MESSAGE( STATUS "\tInclude Directory: ${YAMLCPP_INCLUDE_DIR}" )
+    MESSAGE( STATUS "\tLibraries: ${YAMLCPP_LIBRARY}" )
+ENDIF()
