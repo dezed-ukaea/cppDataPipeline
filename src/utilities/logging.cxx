8a1419a7521cbe427b58488d7ff025bba2d51881
Derek Sandiford <derek.sandiford@ukaea.uk>
2022-02-15 22:00:34 +0000

    Better global APILogger declaration and creation

--- src/utilities/logging.cxx
+++ src/utilities/logging.cxx
@@ -1,11 +1,20 @@
 #include "fdp/utilities/logging.hxx"
 
 namespace FDP {
-std::shared_ptr<spdlog::logger> create_logger_() {
-  if (!spdlog::get("FDPAPI")) {
-    return spdlog::stdout_color_st("FDPAPI");
-  } else {
-    return spdlog::get("FDPAPI");
-  }
-}
+
+    /*! **************************************************************************
+     * @brief Create a the global logger pointer
+     * @author K. Zarebski (UKAEA)
+     *
+     * @return a shared pointer to the global logger instance
+     ****************************************************************************/
+    static logger::sptr create_logger() {
+        if (!spdlog::get("FDPAPI")) {
+            return spdlog::stdout_color_st("FDPAPI");
+        } else {
+            return spdlog::get("FDPAPI");
+        }
+    }
+
+    const logger::sptr APILogger = create_logger();
 } // namespace FDP
