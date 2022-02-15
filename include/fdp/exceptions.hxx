275f5d986b127c99fd08b9de028b0302f6b01a31
Derek Sandiford <derek.sandiford@ukaea.uk>
2022-02-15 21:59:27 +0000

    exceptional smells

--- include/fdp/exceptions.hxx
+++ include/fdp/exceptions.hxx
@@ -7,34 +7,34 @@
 namespace FDP {
 class config_parsing_error : public std::runtime_error {
 public:
-  config_parsing_error(const std::string message)
+  config_parsing_error(const std::string& message)
       : std::runtime_error(message) {}
 };
 
 class rest_apiquery_error : public std::runtime_error {
 public:
-  rest_apiquery_error(const std::string message)
+  rest_apiquery_error(const std::string& message)
       : std::runtime_error(message) {}
 };
 
 class json_parse_error : public std::runtime_error {
 public:
-  json_parse_error(const std::string message) : std::runtime_error(message) {}
+  json_parse_error(const std::string& message) : std::runtime_error(message) {}
 };
 
 class validation_error : public std::runtime_error {
 public:
-  validation_error(const std::string message) : std::runtime_error(message) {}
+  validation_error(const std::string& message) : std::runtime_error(message) {}
 };
 
 class sync_error : public std::runtime_error {
 public:
-  sync_error(const std::string message) : std::runtime_error(message) {}
+  sync_error(const std::string& message) : std::runtime_error(message) {}
 };
 
 class write_error : public std::runtime_error {
 public:
-  write_error(const std::string message) : std::runtime_error(message) {}
+  write_error(const std::string& message) : std::runtime_error(message) {}
 };
 
 }; // namespace FDP
